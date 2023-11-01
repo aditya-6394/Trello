e9d0c65af1e4a69a1c5c28625af30bbe1157786a
Aditya Singh <aditya6394@outlook.com>
2023-11-01 13:05:16 +0530

    Changes

--- src/components/errorAndLoading/ErrorBoundary.jsx
+++ src/components/errorAndLoading/ErrorBoundary.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import { ErrorBoundary } from "react-error-boundary";
 
-function ErrorFallback({ error, resetErrorBoundary }) {
+function ErrorFallback({ error }) {
   return (
     <div>
       <h2>Something went wrong</h2>
       <p>{error.message}</p>
-      <button onClick={resetErrorBoundary}>Try again</button>
     </div>
   );
 }
